2b7d53fc82197dd3d735967fb7ca5e2d9aeb1b0c
Giuliano Annunziata <giuliano.annunziata@outlook.com>
2017-01-27 10:33:36 +0000

    added savePrincess method to Player called by collectItem

--- src/org/academiadecodigo/bootcamp/Gauntlet/gameObject/movableObjects/Player.java
+++ src/org/academiadecodigo/bootcamp/Gauntlet/gameObject/movableObjects/Player.java
@@ -11,7 +11,9 @@ import org.academiadecodigo.bootcamp.Gauntlet.grid.position.GridPosition;
  * Created by s7r1d3r on 20-01-2017.
  */
 public class Player extends Character {
+
     private int points;
+    private boolean hasPrincess;
 
     public Player(Grid grid, GridPosition position) {
         super(grid, position, GameObjType.PLAYER, 3); //TODO: change speed? (current is 3)
@@ -25,5 +27,9 @@ public class Player extends Character {
     @Override   // TODO => JOAQUIM
     public void move() {
 
+    }
+
+    public void savePrincess() {
+        hasPrincess = true;
     }
 }
